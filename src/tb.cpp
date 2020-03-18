83f430af7e8da8f9c733ac368fa5d1c3ab388793
jebbailey <baileyji@ucsb.edu>
2020-03-18 14:45:39 -0700

    Remove delay lines. Change pattern to Lane 0: 0 0 16 16 32 32 Lane 1: 8 8 24 24 40 40 ...

--- src/tb.cpp
+++ src/tb.cpp
@@ -4,9 +4,10 @@
 #include "hls_stream.h"
 using namespace std;
 
-#define N_CYCLES 3
+#define N_CYCLES 2
 #define __PRINT_PATTERN__ 1
-#define PRINT_LANES 20
+#define PRINT_LANES 3
+#define TEST_OUT
 
 int main(){
 
@@ -27,28 +28,29 @@ int main(){
 	//Run the stream input
 	for (int i=0; i<N_CYCLES;i++) { // Go through more than once to see the phase increment
 		for (int j=0;j<N_GROUPS;j++) { //takes N_RES_GROUPS cycles to get through each resonator once
-			adc_to_opfb(istream[i*N_GROUPS+j], qstream[i*N_GROUPS+j], lane[i*N_GROUPS+j]);
+			int ndx = i*N_GROUPS+j - 1;
+			adc_to_opfb(istream[i*N_GROUPS+j], qstream[i*N_GROUPS+j], lane[ndx<0 ? 0: ndx]);
 		}
 	}
 
 	//Compare the result
 	for (int k=0; k<N_LANES; k++){
 		for (int i=0; i<N_CYCLES;i++) { // Go through more than once to see the phase increment
-			if (i==0 &&k<PRINT_LANES) cout<<"Lane "<<k<<",";
+			if (i==0 &&k<PRINT_LANES) cout<<"Lane "<<k<<":,";
 			for (int j=0;j<N_GROUPS;j++) { //takes N_RES_GROUPS cycles to get through each resonator once
 
 				//istream[j].data[k/2] odds went into odd k
 				//istream[j].data[k/2] evens went into even k
 				int cycle=N_GROUPS*i+j;
-				bool even_lane=(k+1)%2;
+
+				if (i==N_CYCLES-1 && j==N_GROUPS-1) break;
 				bool odd_lane=k%2;
 				bool odd_cycle=cycle%2;
-				bool even_cycle=(cycle+1)%2;
 
-				int predicted=N_LANES*(cycle-1)/2 + 8*odd_lane + k/2 - 2040*even_cycle;
-				int lanev=lane[cycle][k].data.real();
+				int predicted=N_LANES*(cycle/2) + 8*odd_lane + k/2;
+				int lanev=lane[cycle][k].data;
 
-				if ((k<PRINT_LANES&&i>0)&&!PRINT_LANES) {
+				if ((k<PRINT_LANES)&! __PRINT_PATTERN__) {
 					cout<<"p"<<predicted;
 					if (predicted==lanev) cout<<"==";
 					else cout<<"!=";
@@ -56,10 +58,10 @@ int main(){
 				}
 
 				#ifdef __PRINT_PATTERN__
-					if (k<PRINT_LANES&i>0) cout<<lanev<<", ";
+					if (k<PRINT_LANES) cout<<lanev<<", ";
 				#endif
 
-				fail|= (predicted!=lanev&&i>0);
+				fail|= (predicted!=lanev);
 			}
 		}
 		if (k<PRINT_LANES) cout<<"\n";
--- src/toplevel.cpp
+++ src/toplevel.cpp
@@ -2,7 +2,7 @@
 
 void adc2iq(adcaxis_t iin, adcaxis_t qin, iq_t iq[N_ADC_OUT]) {
 //Package raw ADC samples into complex fixed point IQ values
-#pragma HLS INLINE off
+//#pragma HLS INLINE off
 
 //Making the inputs pass by reference (regardless of packing the temp variables) moves this
 // function from a latency of 1 to 3 and slightly increases resource use (makes sense). It also
@@ -19,52 +19,45 @@ void adc2iq(adcaxis_t iin, adcaxis_t qin, iq_t iq[N_ADC_OUT]) {
 	}
 }
 
-void lane_delay_interleave(iq_t in, iq_t &out, iq128delay_t &delay_line, bool get_delayed) {
-//This function takes in an IQ and returns an IQ. If get_delayed is set it returns the last
-// value from the delay line. If get_delayed is not set the function both shifts the input
-// into the delay line and passes the input to the output.
-#pragma HLS INLINE
-	iq_t delay_iq = delay_line.shift(in, 127,!get_delayed);
-	out = get_delayed ? delay_iq:in;
-}
 
 void process_lanes(iq_t iqs[N_ADC_OUT], pfbaxisin_t lane[N_LANES]) {
 #pragma HLS pipeline ii=1
+	static bool primed;
 	static ap_uint<8> cycle;
-	static iq128delay_t delays[N_LANES];
 	static iq_t even_lane_z1[N_LANES/2];
 #pragma HLS ARRAY_PARTITION variable=even_lane_z1 complete
+	static iq_t last[N_LANES];
+#pragma HLS ARRAY_PARTITION variable=last complete
 //#pragma HLS INTERFACE ap_ctrl_none port=return
 
-	iq_t iq[N_ADC_OUT];
-#pragma HLS ARRAY_PARTITION variable=iq complete
-
-	readiqs: for (int i=0;i<N_ADC_OUT;i++) {
-#pragma HLS UNROLL
-		iq[i]=iqs[i];
-}
 
 	eachlane: for (int i=0;i<N_LANES;i++) {
 #pragma HLS UNROLL
 		bool odd_cycle = cycle[0];
 		bool odd_lane = ap_uint<1>(i);
 		bool run_lane;
-		iq_t iq_out;
+		iq_t iq_out, laneiq;
 
+		laneiq=iqs[i/2];
 		//Odd lanes get odd cycles, even lanes get even cycles
-		run_lane = odd_cycle ? odd_lane : !odd_lane;
-		lane_delay_interleave(iq[i/2], iq_out, delays[i], !run_lane); //update every other lane
+		iq_out = odd_cycle == odd_lane ? laneiq:last[i];
+		if (odd_cycle == odd_lane) last[i]=laneiq;
 
 		//Delay to get things back in sync
 		if (!odd_lane) {
-			lane[i].data = even_lane_z1[i/2];
-			lane[i].last=cycle==255;
+			//if (primed) {
+				lane[i].data = even_lane_z1[i/2];
+				lane[i].last=cycle==255;
+			//}
 			even_lane_z1[i/2]=iq_out;
 		} else {
-			lane[i].data=iq_out;
-			lane[i].last=cycle==255;
+			//if (primed) {
+				lane[i].data=iq_out;
+				lane[i].last=cycle==255;
+			//}
 		}
 	}
+	//primed=true;
 	cycle++;
 }
 
